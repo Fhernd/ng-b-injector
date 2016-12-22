acd121d17451c9d7d9dbbc9da9b8bcc4f91016a4
John Ortiz Ordoñez <johnortizo@outlook.com>
2016-12-22 15:33:34 +0000

    Dependency Injection Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ReflectiveInjector } from '@angular/core';
+import {
+  MyService
+} from './services/MyService';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <button (click)="invokeService()">Get Value</button>
+  `
 })
 export class AppComponent {
-  title = 'app works!';
+  myService: MyService;
+  
+  constructor(){
+    let injector: any = ReflectiveInjector.resolveAndCreate([MyService]);
+    this.myService = injector.get(MyService);
+    console.log('Same instance?: ', this.myService === injector.get(MyService));
+  }
+
+  invokeService() : void {
+    console.log('MyService returned: ', this.myService.getValue());
+  }
 }
---
+++ src/app/services/MyService.ts
@@ -0,0 +1,5 @@
+export class MyService{
+    getValue() : string {
+        return 'A value';
+    }
+}
